f9cf66714a15707da3920323bfad667396a583b3
Evgeniy <evgeniy.migun@mail.ru>
2025-09-07 19:55:59 +0200

    Сильно переработал условия хода игроков

--- mod.py
+++ mod.py
@@ -14,14 +14,14 @@ def print_board(board):
 def check_winner(board):
     for row in board:
         if row[0] == row[1] == row[2] != ' ':
-            return row[0]
+            return True
     for column in range(3):
         if board[0][column] == board[1][column] == board[2][column] != ' ':
-            return board[0][column]
+            return True
     if board[0][0] == board[1][1] == board[2][2] != ' ':
-        return board[1][1]
+        return True
     if board[0][2] == board[1][1] == board[2][0] != ' ':
-        return board[1][1]
+        return True
     return None
 
 
@@ -38,32 +38,38 @@ def play(board = None):
         raise 'Нужна таблица!'
     
     while True:
-
-        if check_draw(board):
-            print('Ничья!')
-            return
+    
         print_board(board)
-        players_1 = int(input('Игрок 1. Введите ряд 1-3: ')) -1
-        players_2 = int(input('Игрок 1. Введите позицию 1-9: ')) -1
+        player_x = int(input('Игрок №1 - X. Введите номер клетки 1-9: '))
         
-        board[players_1][players_2] = 'X'
+        if player_x > 0 and player_x < 4:
+            board[0][player_x - 1] = 'X'
+        elif player_x < 7:
+            board[1][player_x - 4] = 'X'
+        elif player_x < 10:
+            board[2][player_x - 7] = 'X'
         print_board(board)
         
-        
-        print_board(board)
-
         if check_winner(board):
             print_board(board)
-            print('Игрок №1 = X победил!')
+            print('Игрок №1 победил!')
             break
-
-        player_3 = int(input('Игрок 2. Введите ряд 1-3: ')) -1
-        player_4 = int(input('Игрок 2. Введите позицию 1-9: ')) -1
-        board[player_3][player_4] = 'Y'
-       
+        elif check_draw(board):
+            print('Игра закончилась вничью!')
+        
+        player_o = int(input('Игрок №2 - O. Введите номер клетки 1-9: '))
+        if player_o > 0 and player_o < 4:
+            board[0][player_o - 1] = 'O'
+        elif player_o < 7:
+            board[1][player_o - 4] = 'O'
+        elif player_o < 10:
+            board[2][player_o - 7] = 'O'
+        print_board(board)
         if check_winner(board):
             print_board(board)
-            print('Игрок №2 = Y победил!')
-            return
+            print('Игрок №2 победил!')
+            break
+        elif check_draw(board):
+            print('Игра закончилась вничью!')
 
 play(board)
